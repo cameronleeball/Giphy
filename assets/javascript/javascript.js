c773495363c8743aeffc3f65fc705ef0e4e812a1
Michael Neuman <mpneuman@Michaels-MacBook-Pro.local>
2018-01-29 15:56:16 -0500

    uodates to remove button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {
-// An array of animals, new animals will be pushed into this array;
-var animals = ["African Grey", "Dalmatian", "Cats", "Octopus", "Mantis Shrimp", "Fish", "Sailfin Tang", "Goat","Cicken", "Horse", "Goby"];
-// Creating Functions & Methods
-// Function that displays all gif buttons
+
+var animals = ["African Grey", "Dalmatian", "Cats", "Octopus", "Mantis Shrimp", "Fish", "Sailfin Tang", "Goat","Chicken", "Horse", "Goby"];
+
 function displayGifButtons(){
-    $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+    $("#gifButtonsView").empty();
     for (var i = 0; i < animals.length; i++){
         var gifButton = $("<button>");
         gifButton.addClass("animal");
@@ -14,12 +13,12 @@ function displayGifButtons(){
         $("#gifButtonsView").append(gifButton);
     }
 }
-// Function to add a new animal button
+
 function addNewButton(){
     $("#addGif").on("click", function(){
     var animal = $("#animal-input").val().trim();
     if (animal == ""){
-      return false; // added so user cannot add a blank button
+      return false; 
     }
     animals.push(animal);
 
@@ -27,9 +26,7 @@ function addNewButton(){
     return false;
     });
 }
-// Function to remove last animal button
-    // Doesnt work properly yet removes all of the added buttons
-    // rather than just the last
+
 function removeLastButton(){
     $("removeGif").on("click", function(){
     animals.pop(animal);
@@ -37,11 +34,11 @@ function removeLastButton(){
     return false;
     });
 }
-// displays all of the gifs
+
 function displayGifs(){
     var animal = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-    console.log(queryURL); // displays URL
+    console.log(queryURL); 
     $.ajax({
         url: queryURL,
         method: 'GET'
@@ -49,36 +46,34 @@ function displayGifs(){
     .done(function(response) {
         console.log(response); 
         $("#gifsView").empty(); 
-        var results = response.data; //shows results of gifs
+        var results = response.data; 
         if (results == ""){
           alert("There isn't a gif for this selected button");
         }
         for (var i=0; i<results.length; i++){
 
-            var gifDiv = $("<div>"); //div for gifs 
+            var gifDiv = $("<div>"); 
             gifDiv.addClass("gifDiv");
-            // gif rating
+           
             var gifRating = $("<p>").text("Rating: " + results[i].rating);
             gifDiv.append(gifRating);
-            // pulling gif
+           
             var gifImage = $("<img>");
-            gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
-            gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
-            gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
-            gifImage.attr("data-state", "still"); // set the image state
+            gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
+            gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
+            gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+            gifImage.attr("data-state", "still"); 
             gifImage.addClass("image");
             gifDiv.append(gifImage);
-            // pulling still image 
-            // adding div of gifs to gifsView div
             $("#gifsView").prepend(gifDiv);
         }
     });
 }
-// Functions & Methods
-displayGifButtons(); // displays list of animals already created
+
+displayGifButtons(); 
 addNewButton();
 removeLastButton();
-// Event Listeners
+
 $(document).on("click", ".animal", displayGifs);
 $(document).on("click", ".image", function(){
     var state = $(this).attr('data-state');
